da3ebe32c69ff787086e4e3f6e4b764cc0643776
BJohnson8907 <BrianJ890104@gmail.com>
2022-04-27 18:00:16 -0700

    our first commit

---
+++ quiz.js
@@ -0,0 +1,43 @@
+//this quiz will test knowlege of js variables and how to properly use them//
+
+//question #1 part1 Use the var keyword to declare a variable called mydomain. 
+//question #1 part2 Next assign a number to your new variable.//
+//type answer below this line//
+
+//type answer above this line//
+
+//question #2 Assign the contents of variable X to Z//
+var x;
+x = 7;
+var z;
+//type answer below this line//
+
+//type answer above this line//
+
+//question #3 Why does the code result in an error?//
+let toyName = "transformer";
+let toyName = "lets roll!";
+//Fix code below this line so it does not result in error//
+
+//Fix code above this line so it does not result in error//
+
+//question #4 Which Variables are undefined//
+var q;
+var r;
+var s;
+
+q = q + 1;
+r = r + 5;
+s = s + " String!";
+//change code below this line so undefined variables are defined//
+
+//change code above this line so undefined variables are defined//
+
+//question #5 Replace var with a different variable and check the code for any additional errors//
+var nFL = "nationalfootballleauge";
+var score = "touchdown!"; 
+score = "endzone!";
+console.log(nFL, score); 
+//rewrite code bellow this line without errors//
+
+//rewrite code above this line without errors//
